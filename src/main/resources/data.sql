0665850b264ed5c63f2369a723df132af3618831
ahmetburakozturk <ozturkaburak@gmail.com>
2022-08-31 17:08:08 +0300

    postman configuration

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,6 @@
-insert into public.office (name, location)
-values ('XYZ Ankara Office', 'Turkey/Ankara');
-insert into public.office (name, location)
-values ('XYZ Istanbul Office', 'Turkey/Istanbul');
+insert into public.office (name, location) values ('XYZ Ankara Office', 'Turkey/Ankara');
+insert into public.office (name, location) values ('XYZ Istanbul Office', 'Turkey/Istanbul');
 
-insert into public.department (name)
-values ('HR');
-insert into public.department (name)
-values ('Engineering');
-insert into public.department (name)
-values ('Finance');
+insert into public.department (name) values ('HR');
+insert into public.department (name) values ('Engineering');
+insert into public.department (name) values ('Finance');
